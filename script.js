6023364959676027e27e73350c6ddedd2a8cd72d
abhishekpanditt <123313123+abhishekpanditt@users.noreply.github.com>
2023-07-18 12:58:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,31 @@
 function threeSum(arr, target) {
 // write your code here
-  
+	  
+	 arr.sort((a,b)=>a-b)
+
+        // console.log(arr)
+        let len = arr.length
+        let closestSum = 10000000;
+        for(let i=0;i<=len-3;i++){
+            // len-1
+            // len-2
+            // len-3
+            let left = i+1;
+            let right = len-1;
+
+            while(left<right){
+                let currSum = arr[i]+arr[left]+arr[right]
+
+                if(Math.abs(currSum-target)<Math.abs(closestSum-target))
+                    closestSum = currSum
+
+                if(currSum>target)
+                    right--
+                else
+                    left++
+            }
+        }
+        return closestSum;
 }
 
 module.exports = threeSum;
